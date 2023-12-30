8a907a1c61b99581ac0d4ba21dbfcac53f4d1c9a
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-12-30 17:07:17 +1030

    build: initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.SendEmailRequest;
+import com.resend.services.emails.model.SendEmailResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ResendException {
+        Resend resend = new Resend("re_ddL5T5Jf_9KS6nyvmRWBy2PRFEuLkCG1Q");
+
+        SendEmailRequest sendEmailRequest = SendEmailRequest.builder()
+                .from("onboarding@resend.dev")
+                .to("jhonatan.ibatac@gmail.com")
+                .subject("Hello World")
+                .html("<p>Congrats on sending your <strong>first email</strong>!</p>")
+                .build();
+
+        SendEmailResponse data = resend.emails().send(sendEmailRequest);
     }
 }
